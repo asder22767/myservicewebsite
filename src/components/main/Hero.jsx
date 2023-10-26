62a90a6d2ea1d2a69fce9a39faad016bbea30d2f
00016221 <wiutstudent00016096@gmail.com>
2023-10-25 22:44:00 -0700

    ...

--- src/components/main/Hero.jsx
+++ src/components/main/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
         <div className="container mx-auto">
           <div className="hero__wrapper pt-20">
             <h2 className="hero__heading font-merriweather font-bold text-darkblue text-[64.8px] leading-[86.3px] text-center mt-20">
-              Coming Soon
+              Coming Soon...
             </h2>
           </div>
         </div>
